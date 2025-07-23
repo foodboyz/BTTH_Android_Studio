461d76e7d85ac4aaa71cff5f0b5fd5274a2895b8
foodboyz <vinhtlustudy1892004@gmail.com>
2025-07-23 23:20:46 +0700

    Demo_Cycle_Life

--- bai2/app/src/main/java/com/example/bai2/MainActivity.java
+++ bai2/app/src/main/java/com/example/bai2/MainActivity.java
@@ -1,9 +1,6 @@
-package com.example.bai1;
+package com.example.bai2;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +10,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edt_input1, edt_input2, edt_ketQua;
-    Button btn_sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,20 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        edt_input1 = findViewById(R.id.edt_input1);
-        edt_input2 = findViewById(R.id.edt_input2);
-        edt_ketQua = findViewById(R.id.edt_ketQua);
-        btn_sum = findViewById(R.id.btn_sum);
-
-        btn_sum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int a = Integer.parseInt(edt_input1.getText().toString());
-                int b = Integer.parseInt(edt_input2.getText().toString());
-                int c = a + b;
-                edt_ketQua.setText(String.valueOf(c));
-            }
         });
     }
 }
